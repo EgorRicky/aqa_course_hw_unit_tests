25e1d5dd028b595e240011ec377ee9a9c8af96e0
Egor Shein <egor.shein@philipa.tech>
2025-07-28 18:56:27 +0800

    added hw-3 task1 and task2

--- homework-solutions/js/hw-3/task-2.js
+++ homework-solutions/js/hw-3/task-2.js
@@ -4,3 +4,11 @@
 n + nn + nnn, где n не перемножаются, а конкатенируются
 
 */
+
+const n = 2;
+const moveToString = String(n);
+const secondNum = Number(moveToString + moveToString);
+const thirdNum = Number(moveToString + moveToString + moveToString);
+console.log(n, secondNum, thirdNum);
+const finalRes = n + secondNum + thirdNum;
+console.log(finalRes);
