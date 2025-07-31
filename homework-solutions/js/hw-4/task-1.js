2e5e86c1331a037a66faf5526e69c32581035221
Egor Shein <egor.shein@philipa.tech>
2025-08-01 07:54:57 +0800

    added 3 tasks for hw-2 of js topic

--- homework-solutions/js/hw-4/task-1.js
+++ homework-solutions/js/hw-4/task-1.js
@@ -3,3 +3,20 @@
  - Создайте переменную salary со значением 1000
   - Создайте переменную grade, которая должна получить значение "middle" если salary больше или равна 1000, и значение "junior" - если меньше
   */
+
+const salary = 1000;
+let grade;
+
+if (salary >= 1000) {
+    grade = "middle";
+    console.log(grade);
+}
+
+else if (salary < 1000) {
+    grade = "junior";
+    console.log(grade)
+}
+
+else {
+    console.log('grade was not identified')
+}
