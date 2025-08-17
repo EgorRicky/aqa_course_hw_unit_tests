b7a25a0f210475b182ffadd586e39174005468a3
Egor Shein <egor.shein@philipa.tech>
2025-08-17 12:37:23 +0800

    1st task

--- homework-solutions/js/hw-7/task-1.mjs
+++ homework-solutions/js/hw-7/task-1.mjs
@@ -15,7 +15,26 @@ function mergeArrays() {
     - Пример: I am super engineer => i_Am_Super_Engineer
   */
 function devideBy(sentence) {
-  // Ваш код
+  const arrOfWords = sentence.split(' ');
+  const arrOfWordsFiltered = [];
+  for (const arrOfWord of arrOfWords) {
+    if (arrOfWord) {
+      arrOfWordsFiltered.push(arrOfWord);
+    }
+  }
+  const toLowerCaseWords = [];
+  for (const word of arrOfWordsFiltered) {
+    toLowerCaseWords.push(word.toLowerCase());
+  }
+  const wordsStartWithUpperCase = [];
+  for (let i = 1; i < toLowerCaseWords.length; i++) {
+    const newWord = toLowerCaseWords[i][0].toUpperCase() + toLowerCaseWords[i].slice(1).toLowerCase();
+    wordsStartWithUpperCase.push(newWord);
+  }
+  const newSentenceArr = [toLowerCaseWords[0], ...wordsStartWithUpperCase];
+  const newSentence = newSentenceArr.join('_');
+  console.log(newSentence);
+  return newSentence;
 }
 /*
   3. Фибаначчи
